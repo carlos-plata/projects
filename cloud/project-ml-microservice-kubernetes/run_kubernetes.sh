e970ac24b3f05bca3aeee0ab9330b800262a80a8
Mckay <Mckay@10.0.0.51>
2020-05-04 23:11:49 -0400

    updated kubectl

--- cloud/project-ml-microservice-kubernetes/run_kubernetes.sh
+++ cloud/project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -8,13 +8,13 @@ dockerpath=charliesilver/udacitymicroservice
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-microk8s.kubectl run udacitymicroservice --image=charliesilver/udacitymicroservice --port=80
+kubectl run udacitymicroservice --image=charliesilver/udacitymicroservice --port=80
 
 # Step 3:
 # List kubernetes pods
-microk8s.kubectl get pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-microk8s.kubectl port-forward deployment/udacitymicroservice 8000:80 
-microk8s.kubectl logs --selector app=udacitymicroservice
+kubectl port-forward deployment/udacitymicroservice 8000:80 
+kubectl logs --selector app=udacitymicroservice
